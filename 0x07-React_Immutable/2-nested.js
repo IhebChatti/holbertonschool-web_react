9f02f0c9263cf333dc49f2795e6f1b2bd43b83cd
IhebChatti <iheb.chatti@holbertonschool.com>
2021-07-28 20:13:39 +0100

    nested

--- 0x07-React_Immutable/2-nested.js
+++ 0x07-React_Immutable/2-nested.js
@@ -1,7 +1,7 @@
 import { fromJS } from 'immutable';
 
 const accessImmutableObject = (object, array) => {
-	const mappedObj = fromJS(object);
+  const mappedObj = fromJS(object);
   return mappedObj.getIn(array, undefined);
 };
 
